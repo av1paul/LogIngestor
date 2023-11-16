430c0b71dc80491991e250ad984c02c00cabe676
Avishek Paul <57027258+av1paul@users.noreply.github.com>
2023-11-16 22:20:16 +0530

    initial setup

---
+++ src/main/java/org/avi/LogIngestorApplication.java
@@ -0,0 +1,11 @@
+package org.avi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogIngestorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogIngestorApplication.class, args);
+    }
+}
